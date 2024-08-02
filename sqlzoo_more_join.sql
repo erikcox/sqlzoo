3ebbe9d5b47481970138083b026a3978935f8bf9
Erik <erikbcox@gmail.com>
2024-08-02 12:38:30 -0700

    Completed sqlzoo_more_join.sql

--- sqlzoo_more_join.sql
+++ sqlzoo_more_join.sql
@@ -43,24 +43,44 @@ WHERE movie.title = 'Alien';
 WITH hmovies AS (SELECT movieid FROM casting WHERE actorid = (SELECT id FROM actor WHERE name = 'Harrison FORD'))
 SELECT title FROM movie WHERE id IN (SELECT movieid FROM hmovies);
 -- Alternate, using JOIN
-SELECT title FROM movie 
+SELECT title 
+FROM movie 
 JOIN  casting ON movie.id = casting.movieid
 JOIN  actor ON actor.id  = casting.actorid
-WHERE casting.actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford');
+WHERE actor.name = 'Harrison Ford';
 
 -- 9. List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
 SELECT title FROM movie 
 JOIN  casting ON movie.id = casting.movieid
 JOIN  actor ON actor.id  = casting.actorid
-WHERE casting.actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford')
+WHERE actor.name = 'Harrison Ford'
 AND casting.ord <> 1;
 
 -- 10. List the films together with the leading star for all 1962 films.
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE movie.yr = 1962
+AND casting.ord = 1;
 
 -- 11. Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+SELECT movie.yr, COUNT(movie.id) AS num_films
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE actor.name = 'Rock Hudson'
+GROUP BY movie.yr
+HAVING COUNT(movie.id) > 2;
 
 -- 12. List the film title and the leading actor for all of the films 'Julie Andrews' played in.
 -- Did you get "Little Miss Marker twice"?
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE  casting.ord = 1
+AND movie.id IN (SELECT movie.id FROM movie);
 
 -- 13. Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
 
